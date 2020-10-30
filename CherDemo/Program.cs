ec59f5d53d20e8adfeb0510a982f23c96edcddef
Donnel <donnel206@hotmail.com>
2020-10-30 10:52:44 +0100

    Add project files.

---
+++ CherDemo/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CharDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //char someLetter = 'a';
+            //char someSymbol = '#';
+            //char anotherOne = '?';
+
+            //Console.WriteLine(someLetter+ "" +);
+
+
+
+
+
+            for (char kar = 'a'; kar <= 'z'; kar++)
+            {
+                Console.WriteLine(kar);          
+            }
+
+            char letter = 'a';
+            Console.WriteLine((int)letter);
+
+        }
+    }
+}
